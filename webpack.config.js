df7f77e3ef5571c1b1047a2f46dfd01ba73395cc
Nyame-Wolf <mumenyam@gmail.com>
2022-07-06 22:44:06 +0300

    Create to-do list with static data

--- webpack.config.js
+++ webpack.config.js
@@ -2,34 +2,34 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
+  mode: 'development',
+  entry: './src/index.js',
 
-    devtool: 'inline-source-map',
-    devServer: {
-        static: './dist',
-    },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
 
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-        }),
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-        ],
-    },
-    optimization: {
-        runtimeChunk: 'single',
-    },
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
 };
