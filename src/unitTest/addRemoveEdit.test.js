2a7759f8c551be8bdfd82fbaf095fc95ce0bde9f
wayungi <fwayungi@gmail.com>
2022-07-15 23:23:24 +0300

    Add test to remove a todo item

--- src/unitTest/addRemoveEdit.test.js
+++ src/unitTest/addRemoveEdit.test.js
@@ -2,32 +2,40 @@
  * @jest-environment jsdom
  */
 
-import { addToDo, getToDos } from "../modules/addRemoveEdit.js";
+import {
+  addToDo,
+  getToDos,
+  removeTodos,
+  setToDos,
+  refreshToDos,
+} from '../modules/addRemoveEdit.js';
 
-describe("Test todo app", () => {
+describe('Test todo app', () => {
   const task1 = {
-    description: "Play soccer",
+    description: 'Play soccer',
     completed: false,
-    index: "1",
+    index: 1,
   };
 
-  //   const task2 = {
-  //     description: "Eat food",
-  //     completed: false,
-  //     index: "2",
-  //   };
+  console.log(task1.index);
 
-  //   const task3 = {
-  //     description: "Take a nap",
-  //     completed: false,
-  //     index: "3",
-  //   };
+  const task2 = {
+    description: 'Eat food',
+    completed: false,
+    index: 2,
+  };
 
-  //   const task4 = {
-  //     description: "pair program",
-  //     completed: false,
-  //     index: "4",
-  //   };
+  const task3 = {
+    description: 'Take a nap',
+    completed: false,
+    index: 3,
+  };
+
+  const task4 = {
+    description: 'pair program',
+    completed: false,
+    index: 4,
+  };
 
   //   const task5 = {
   //     description: "Drink water",
@@ -42,13 +50,32 @@ describe("Test todo app", () => {
   //     index: "6",
   //   };
 
-  describe("add todo", () => {
-    test("should add task 1 to array", () => {
+  describe('add todo', () => {
+    test('should add task 1 to array', () => {
       // act
       addToDo(task1);
       // assert
       expect(getToDos().length).toEqual(1);
       expect(getToDos()[0]).toEqual(task1);
+    });
+  });
+
+  describe('remove todo', () => {
+    test('Should remove tasks from array', () => {
+      // act
+      addToDo(task2);
+      addToDo(task3);
+      addToDo(task4);
+
+      const newTodos = removeTodos(
+        (task2) => task2.index !== getToDos()[1].index,
+        getToDos()
+      );
+      setToDos(newTodos);
+
+      // assert
+      expect(getToDos().length).toEqual(3);
+      expect(getToDos()).not.toContain(task2);
     });
   });
 });
